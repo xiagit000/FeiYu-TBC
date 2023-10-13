3b24f7c3902063d4d595ed28d81b257f40d83a14
Niam5 <ebakkedahl@gmail.com>
2023-10-13 16:15:46 -0500

    Multiple Eluna changes and additions
    -New hooks for Player, Item, and Trade(Init and Accept) added to core
    -Exclude VMangos header directory
    -OnDummyEffect hooks added
    -Update Eluna submodule
    
    Co-Authored-By: Foereaper <2131987+foereaper@users.noreply.github.com>

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -3587,6 +3587,9 @@ void Player::learnSpell(uint32 spell_id, bool dependent, bool talent)
         WorldPacket data(SMSG_LEARNED_SPELL, 4);
         data << uint32(spell_id);
         GetSession()->SendPacket(data);
+#ifdef BUILD_ELUNA
+        sEluna->OnLearnSpell(this, spell_id);
+#endif
     }
 
     // learn all disabled higher ranks (recursive) - skip for talent spells
@@ -7020,6 +7023,9 @@ uint32 Player::GetLevelFromDB(ObjectGuid guid)
 
 void Player::UpdateArea(uint32 newArea)
 {
+#ifdef BUILD_ELUNA
+    uint32 oldArea = m_areaUpdateId;
+#endif
     m_areaUpdateId    = newArea;
 
     AreaTableEntry const* area = GetAreaEntryByAreaID(newArea);
@@ -7040,6 +7046,12 @@ void Player::UpdateArea(uint32 newArea)
     }
 
     UpdateAreaDependentAuras();
+
+#ifdef BUILD_ELUNA
+    // We only want the hook to trigger when the old and new area is actually different
+    if (oldArea != newArea)
+        sEluna->OnUpdateArea(this, oldArea, newArea);
+#endif
 }
 
 bool Player::CanUseCapturePoint() const
@@ -10391,6 +10403,10 @@ Item* Player::StoreNewItem(ItemPosCountVec const& dest, uint32 item, bool update
     {
         ItemAddedQuestCheck(item, count);
         pItem = StoreItem(dest, pItem, update);
+
+#ifdef BUILD_ELUNA
+        sEluna->OnAdd(this, pItem);
+#endif
     }
     return pItem;
 }
@@ -14175,6 +14191,10 @@ void Player::SetQuestStatus(uint32 quest_id, QuestStatus status)
     }
 
     UpdateForQuestWorldObjects();
+
+#ifdef BUILD_ELUNA
+    sEluna->OnQuestStatusChanged(this, quest_id, status);
+#endif
 }
 
 // not used in MaNGOS, but used in scripting code
